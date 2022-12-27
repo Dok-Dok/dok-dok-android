ed850ac19ad8f94de0fa864a3e97f0d5f6dd7415
최민재 <cmjcmj1230@gmail.com>
2022-12-27 09:08:15 +0900

    ✅ :: Init project

---
+++ app/src/main/java/kr/hs/minjae/dokdok/MainActivity.kt
@@ -0,0 +1,41 @@
+package kr.hs.minjae.dokdok
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.hs.minjae.dokdok.ui.theme.DokDokTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DokDokTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DokDokTheme {
+        Greeting("Android")
+    }
+}
